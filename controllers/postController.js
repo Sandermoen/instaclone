4a9c8a13eedf596ffa7efbf3848f48f503629ddd
sander <smoensander@gmail.com>
2021-06-26 16:17:50 +0200

    Change explicit content error message

--- controllers/postController.js
+++ controllers/postController.js
@@ -48,8 +48,7 @@ module.exports.createPost = async (req, res, next) => {
     if (response.moderation[0].status === 'rejected') {
       console.log('yes');
       return res.status(403).send({
-        error:
-          'The content was deemed to explicit to upload. Please upload another picture.',
+        error: 'The content was deemed to explicit to upload.',
       });
     }
     const thumbnailUrl = formatCloudinaryUrl(
